52f0b224b16ec40aec6527385d341b25d3668ef3
phatdroid94@gmail.com <phatdroid94@gmail.com>
2021-04-22 21:22:55 +0700

    import data

--- app/src/main/java/com/example/recyclerview23032021/FoodAdapter.java
+++ app/src/main/java/com/example/recyclerview23032021/FoodAdapter.java
@@ -1,14 +1,44 @@
 package com.example.recyclerview23032021;
 
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
-public class FoodAdapter {
+import java.util.List;
 
+public class FoodAdapter extends RecyclerView.Adapter<FoodAdapter.FoodViewHolder>{
+
+    private List<Food> mListFood;
+
+    public FoodAdapter(List<Food> mListFood) {
+        this.mListFood = mListFood;
+    }
+
+    @NonNull
+    @Override
+    public FoodViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        LayoutInflater layoutInflater = LayoutInflater.from(parent.getContext());
+        View v = layoutInflater.inflate(R.layout.layout_item_food,parent,false);
+        return new FoodViewHolder(v);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull FoodViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        if (mListFood != null && mListFood.size() > 0){
+            return mListFood.size();
+        }
+        return 0;
+    }
 
     class FoodViewHolder extends RecyclerView.ViewHolder{
 
--- app/src/main/java/com/example/recyclerview23032021/MainActivity.java
+++ app/src/main/java/com/example/recyclerview23032021/MainActivity.java
@@ -4,11 +4,31 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
 
+    List<Food> mFoodList;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mFoodList = new ArrayList<>();
+
+        mFoodList.add(new Food(
+                true,
+                R.drawable.hinhplaydumsum,
+                "Play Dim Sum - Thái Văn Lung",
+                "12 -14 Thái Văn Lung, P. Bến Nghé, Quận 1",
+                new String[] {
+                        "Cả ngày - Giảm 20%",
+                        "Ăn trưa - Ưu đãi",
+                        "Cả ngày - Tặng 01 phần Bánh Nướng",
+                        "Cả ngày - Đặt bàn"
+                }
+                ));
+
     }
 }
